9bd6bc98cef8e6c35dbe7cd8d677a800fd44f898
IIvanov57 <ivanov080892@mail.ru>
2024-05-29 18:45:02 +0300

    add size parametr

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -13,13 +13,18 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 import static com.codeborne.selenide.Configuration.remote;
+import static com.codeborne.selenide.Configuration.browserSize;
+import static com.codeborne.selenide.Selenide.executeJavaScript;
 
 public class TestBase {
   @BeforeAll
   static void beforeALL() {
     Configuration.pageLoadStrategy = "eager";
     //Configuration.browserSize = "1024x768";
-    Configuration.browserSize = "1920x1080";
+
+    browserSize = System.getProperty("1920x1080", "1920x1080");
+    //browserSize = "1920x1080";
+
     Configuration.timeout = 10000;
     //Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     //System.setProperty("remote","https://localhost");
@@ -30,6 +35,9 @@ public class TestBase {
             "enableVideo", true
     ));
     Configuration.browserCapabilities = capabilities;
+
+    executeJavaScript("$('footer').remove();");
+    executeJavaScript("$('#fixedban').remove();");
 
   }
 
